68033bc7ce093a7df3e3bb02e74aa033a70d46c5
Paul Bartsch <BartschP@fis.ucsb.edu>
2019-05-20 15:12:05 -0700

    first

---
+++ 02-document-text.py
@@ -0,0 +1,49 @@
+# set GOOGLE_APPLICATION_CREDENTIALS=C:\Projects\cloud_vision_ocr\my_gcv.json
+    
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+path = os.path.join(
+    os.path.dirname(__file__),
+    'resources/assemble.JPG')
+
+def detect_document(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
+
+detect_document(path)
---
+++ 04-document-text.py
@@ -0,0 +1,44 @@
+# set GOOGLE_APPLICATION_CREDENTIALS=C:\Projects\cloud_vision_ocr\my_gcv.json
+    
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+path = os.path.join(
+    os.path.dirname(__file__),
+    'resources/assemble.JPG')
+
+def detect_document(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    output = []
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+
+                    output.append(word_text)
+
+    print(" ".join(output))
+
+detect_document(path)
